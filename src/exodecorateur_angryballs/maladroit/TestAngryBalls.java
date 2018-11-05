4f8311e78ff26c85c0fcf07895f880b0d0769dd6
quentinnade <quentin.nade27@gmail.com>
2018-11-05 23:23:18 +0100

    Ménage dans quelques classes

--- src/exodecorateur_angryballs/maladroit/TestAngryBalls.java
+++ src/exodecorateur_angryballs/maladroit/TestAngryBalls.java
@@ -13,7 +13,6 @@ import exodecorateur_angryballs.maladroit.modele.BilleMvtRURebond;
 import exodecorateur_angryballs.maladroit.modele.BilleMvtPesanteurFrottementRebond;
 import exodecorateur_angryballs.maladroit.vues.CadreAngryBalls;
 import exodecorateur_angryballs.maladroit.vues.VueBillard;
-import exodecorateur_angryballs.modele.*;
 
 /**
  * Gestion d'une liste de billes en mouvement ayant toutes un comportement différent

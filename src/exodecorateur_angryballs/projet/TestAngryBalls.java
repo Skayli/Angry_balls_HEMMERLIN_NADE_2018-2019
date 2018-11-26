f5b73c613be035746283b57d516241e7a63fb3a7
quentinnade <quentin.nade27@gmail.com>
2018-11-26 11:42:31 +0100

    Ajout d'un vecteur d'initialisation supplémentaire

--- src/exodecorateur_angryballs/projet/TestAngryBalls.java
+++ src/exodecorateur_angryballs/projet/TestAngryBalls.java
@@ -49,7 +49,7 @@ public class TestAngryBalls {
 		
 		double rayon = 0.05*Math.min(xMax, yMax); // rayon des billes : ici toutes les billes ont le même rayon, mais ce n'est pas obligatoire
 		
-		Vecteur p0, p1, p2, p3, p4, p5, v0, v1, v2, v3, v4, v5;    // les positions des centres des billes et les vecteurs vitesse au démarrage. 
+		Vecteur p0, p1, p2, p3, p4, p5, p6, v0, v1, v2, v3, v4, v5, v6;    // les positions des centres des billes et les vecteurs vitesse au démarrage. 
 		                                                    // Elles vont être choisies aléatoirement
 		
 		Vecteur pesanteur;			// représente la pesanteur appliquée aux billes qui y sont sensibles
@@ -62,6 +62,7 @@ public class TestAngryBalls {
 		p3 = Vecteur.créationAléatoire(0, 0, xMax, yMax);
 		p4 = Vecteur.créationAléatoire(0, 0, xMax, yMax);
 		p5 = Vecteur.créationAléatoire(0, 0, xMax, yMax);
+		p6 = Vecteur.créationAléatoire(0, 0, xMax, yMax);
 		
 		
 		//------------------- création des vecteurs vitesse des billes ---------------------------------
@@ -72,6 +73,7 @@ public class TestAngryBalls {
 		v3 = Vecteur.créationAléatoire(-vMax, -vMax, vMax, vMax);
 		v4 = Vecteur.créationAléatoire(-vMax, -vMax, vMax, vMax);
 		v5 = Vecteur.créationAléatoire(-vMax, -vMax, vMax, vMax);
+		v6 = Vecteur.créationAléatoire(-vMax, -vMax, vMax, vMax);
 		
 		
 		//------------------- Instanciation de la pesanteur --------------------------------------------
@@ -97,7 +99,7 @@ public class TestAngryBalls {
 		
 		//-------------------- Nouvelle bille avec le comportement "pilotée" --------
 		billes.add(new ComportementBillePilotee(new BilleNormale(p5,rayon,v5, Couleur.gray()),souris));
-//		billes.add(new ComportementRepoussee(new BilleNormale(p1,rayon,v1,Couleur.pink()))); //Bille repoussee par les autres billes (Newton inversé)
+//		billes.add(new ComportementRepoussee(new BilleNormale(p6,rayon,v6,Couleur.pink()))); //Bille repoussee par les autres billes (Newton inversé)
 		
 		//---------------------- ici finit la partie à changer ------------------------------------------------------------
 		
